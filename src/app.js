ffad60095083c08d307eeee0f8b394596b4a8fa1
Soheil Zarepoor <soheilzarepoor79@gmail.com>
2023-11-13 16:05:00 +0330

    first commit

--- src/app.js
+++ src/app.js
@@ -1,29 +1,17 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-require('dotenv').config();
-
-const middlewares = require('./middlewares');
-const api = require('./api');
+require("dotenv").config();
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+app.get("/", async (req, res) => {
+    const response = await fetch("https://irarz.com/");
+    const html = await response.text();
+    res.send(html);
 });
-
-app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
 
 module.exports = app;
